bb4af92d4c0e991f9003fd8daf9a971bc17417cb
michaelauderer <mike@silverstone.io>
2018-06-30 19:56:24 -0400

    use reduce, create wrapper function, add better comments

--- index.js
+++ index.js
@@ -4,51 +4,50 @@ const Db = mongodb.Db
 const Collection = mongodb.Collection
 const Cursor = mongodb.Cursor
 
-const getPrototypeProperties = (Class) => {
-  return Object.getOwnPropertyNames(Class.prototype)
+/**
+ * Wraps all functions of a class with a given wrapper function
+ * @param {class} Class The class to wrap
+ * @param {function} wrapperFn The function to wrap the class methods with
+ */
+const wrap = (Class, wrapperFn) => {
+  const methods = Object.getOwnPropertyNames(Class.prototype)
+  const wrapper = methods.reduce((acc, cur) => {
+    acc[cur] = (...args) => wrapperFn(cur)(args)
+    return acc
+  }, {})
+
+  return wrapper
 }
 
-const mongoClientMethods = getPrototypeProperties(MongoClient)
-const dbMethods = getPrototypeProperties(Db)
-const collectionMethods = getPrototypeProperties(Collection)
-const cursorMethods = getPrototypeProperties(Cursor)
-
 const mongonow = (uri) => {
+  // store a promise which resolves when connecting to MongoDB
   const connect = MongoClient.connect(uri)
-  const wrapper = {}
-  // add MongoClient methods
-  mongoClientMethods
-    .forEach(method => 
-      wrapper[method] = (...args) => 
-        connect.then(mongoClient => mongoClient[method](...args)))
-  // add Db methods
-  wrapper.db = (...dbArgs) => {
-    const dbWrapper = {}
-    dbMethods
-      .forEach(method => 
-        dbWrapper[method] = (...args) => 
-          connect.then(mongoClient => mongoClient.db(...dbArgs)[method](...args)))
-    // add Collection methods
-    dbWrapper.collection = (...collectionArgs) => {
-      const collectionWrapper = {}
-      collectionMethods
-        .forEach(method =>
-          collectionWrapper[method] = (...args) =>
-            connect.then(mongoClient => mongoClient.db(...dbArgs).collection(...collectionArgs)[method](...args)))
-      // add Cursor methods
-      collectionWrapper.find = (...findArgs) => {
-        const findWrapper = {}
-        cursorMethods
-          .forEach(method => 
-            findWrapper[method] = (...args) =>
-              connect.then(mongoClient => mongoClient.db(...dbArgs).collection(...collectionArgs).find(...findArgs)[method](...args)))
-        return findWrapper
-      }
-      return collectionWrapper
-    } 
-    return dbWrapper
+
+  // wrap all the main methods of MongoDB
+  const MongoClientWrapper = wrap(MongoClient, (method) => ((args) => connect.then(mongoClient => mongoClient[method](...args))))
+  const DbWrapper = (dbArgs) => wrap(Db, (method) => ((args) => connect.then(mongoClient => mongoClient.db(...dbArgs)[method](...args))))
+  const CollectionWrapper = (dbArgs, collectionArgs) => wrap(Collection, (method) => ((args) => connect.then(mongoClient => mongoClient.db(...dbArgs).collection(...collectionArgs)[method](...args))))
+  const CursorWrapper = (dbArgs, collectionArgs, findArgs) => wrap(Cursor, (method) => ((args) => connect.then(mongoClient => mongoClient.db(...dbArgs).collection(...collectionArgs).find(...findArgs)[method](...args)).catch(console.log)))
+
+  // Note that we have to nest wrappers like this,
+  // because mongodb has some functions which dont return promises.
+  // For every function which doesn't return a promise, we need to wrap it,
+  // to maintain the same API functionality as the mongodb driver.
+  // If we didn't wrap these functions, then `client.db` would return a
+  // promise, which is different from how the mongodb driver works.
+  const mainWrapper = {
+    ...MongoClientWrapper,
+    db: (...dbArgs) => ({
+      ...DbWrapper(dbArgs),
+      collection: (...collectionArgs) => ({
+        ...CollectionWrapper(dbArgs, collectionArgs),
+        find: (...findArgs) =>
+          CursorWrapper(dbArgs, collectionArgs, findArgs)
+      })
+    })
   }
-  return wrapper
+
+  return mainWrapper
 }
 
 module.exports = mongonow
